5b17697a9d8165a1f1d77f21357551aebef53daf
Mathias Skotnes <mathias.skotnes@telenor.no>
2025-06-04 15:56:42 +0200

    Initial commit

---
+++ src/main/kotlin/com/amediabackend/AmediaBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.amediabackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AmediaBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AmediaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/amediabackend/AmediaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amediabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AmediaBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
